9935c9733ef5f0f5cbaec5cda037f0412fb1fc20
vampire <healer7284@gmail.com>
2026-07-03 20:49:39 +0800

    add viz

---
+++ viz.R
@@ -0,0 +1,11 @@
+
+install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("palmerpenguins")
+library(palmerpenguins)
+
+penguins %>%
+  ggplot(aes(x = bill_depth_mm)) +
+  geom_histogram()
+
